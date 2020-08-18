88c55c46c4c89ed2101075c2c4feafac3326319d
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-08-18 20:07:49 +0300

    Add ImageInputList reusable Component

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
-import React from 'react';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import React, { useState } from 'react';
+
+import Screen from './app/components/Screen';
+import ImageInputList from './app/components/ImageInputList';
 
 export default function App() {
-  return <ListingEditScreen></ListingEditScreen>;
+  const [imageUris, setImageUris] = useState([]);
+
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
+  };
+
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
+  };
+
+  return (
+    <Screen>
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+    </Screen>
+  );
 }
